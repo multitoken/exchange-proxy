4311cd340c4baaf99a5190b7f4fb479bdaa52e81
rremizov <rremizov@yandex.ru>
2021-04-27 18:54:45 +0500

    Update migrations

--- migrations/2_deploy_proxy.js
+++ migrations/2_deploy_proxy.js
@@ -1,3 +1,4 @@
+const BRegistry = artifacts.require("BRegistry");
 const ExchangeProxy = artifacts.require("ExchangeProxy");
 const WETH9 = artifacts.require("WETH9");
 
@@ -6,7 +7,11 @@ module.exports = async function(deployer, network, accounts) {
         await deployer.deploy(WETH9);
         await deployer.deploy(ExchangeProxy, WETH9.address);
     } else if (network == 'kovan-fork' || network == 'kovan') {
-        deployer.deploy(ExchangeProxy, '0xd0A1E359811322d97991E03f863a0C30C2cF029C');
+        const WETH = '0xd0A1E359811322d97991E03f863a0C30C2cF029C';
+        const BFactory = '0x0728eA2885818759c4adD023C4B234D3e3fDf209';
+
+        const registry = await deployer.deploy(BRegistry, BFactory)
+        const proxy = await deployer.deploy(ExchangeProxy, WETH);
+        await proxy.setRegistry(BRegistry.address);
     }
-    
 }
