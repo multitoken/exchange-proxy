f5b40dbbd4495903a2283b0b9e3c5a099921cff9
Alex O <mightycron@gmail.com>
2021-08-23 22:04:02 +0500

    Deploy Scripts Simplification

--- migrations/2_deploy_proxy.js
+++ migrations/2_deploy_proxy.js
@@ -5,17 +5,18 @@ const WETH9 = artifacts.require("WETH9");
 module.exports = async function(deployer, network, accounts) {
     if (network == 'development' || network == 'coverage') {
         await deployer.deploy(WETH9);
-        await deployer.deploy(ExchangeProxy, WETH9.address);
-    } else if (network == 'kovan-fork' || network == 'kovan') {
-        const WETH = '0xd0A1E359811322d97991E03f863a0C30C2cF029C';
-        const BFactory = '0x0728eA2885818759c4adD023C4B234D3e3fDf209';
+        const proxy = await deployer.deploy(ExchangeProxy, WETH9.address);
+        await proxy.setRegistry(BRegistry.address);
 
-        const registry = await deployer.deploy(BRegistry, BFactory)
-        const proxy = await deployer.deploy(ExchangeProxy, WETH);
+        deployer.deploy(TTokenFactory);
+        const bFactory = deployer.deploy(BFactory);
+
+        await deployer.deploy(BRegistry, bFactory);
+        const proxy = await ExchangeProxy.deployed();
+        await proxy.setRegistry(BRegistry.address);
+    } else {
+        await deployer.deploy(BRegistry, process.env.BFACTORY_ADDRESS)
+        const proxy = await deployer.deploy(ExchangeProxy, process.env.WETH_ADDRESS);
         await proxy.setRegistry(BRegistry.address);
     }
-
-    await deployer.deploy(BRegistry, process.env.BFACTORY_ADDRESS)
-    const proxy = await deployer.deploy(ExchangeProxy, process.env.WETH_ADDRESS);
-    await proxy.setRegistry(BRegistry.address);
 }
--- migrations/3_test_factories.js
+++
@@ -1,9 +0,0 @@
-const TTokenFactory = artifacts.require("TTokenFactory");
-const BFactory = artifacts.require("BFactory");
-
-module.exports = async function(deployer, network, accounts) {
-    if (network == 'development' || network == 'coverage') {
-        deployer.deploy(TTokenFactory);
-        deployer.deploy(BFactory);
-    }
-}
